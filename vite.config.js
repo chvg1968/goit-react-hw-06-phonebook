fc866a898a59863807237ca97453b802ccf35344
chvg1968 <conradovilla.gmail.com>
2023-04-29 16:21:39 -0500

    Test2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://chvg1968.github.io/goit-react-hw-02-phonebook/'
 })
