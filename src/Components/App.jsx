ede4b0167d01a67d2041bb646530d95a698c6222
chvg1968 <conradovilla.gmail.com>
2023-04-29 19:22:49 -0500

    Update app

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import data from './data.json';
 import ContactForm from './ContactForm'; 
 import SearchFilter from './SearchFilter';
