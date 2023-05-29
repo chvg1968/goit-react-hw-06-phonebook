05351e2f49d67c80c7712ec658949cf8b738cc2e
chvg1968 <conradovilla.gmail.com>
2023-05-29 10:18:24 -0500

    Update file name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://chvg1968.github.io/goit-react-hw-02-phonebook/'
+    base: 'https://chvg1968.github.io/goit-react-hw-06-phonebook/'
 })
