8e56824acde72f0c2c804b0bc639076667312af5
chvg1968 <conradovilla.gmail.com>
2023-06-01 13:59:25 -0500

    Update ContactForm

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,21 +1,26 @@
 import { useDispatch } from 'react-redux';
 import { addContacts } from '../redux/contactSlice';
+import { selectContacts} from '../redux/selectors';
 import { useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import Swal from 'sweetalert2'
 
 function ContactForm() {
+
+  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const nameInputRef = useRef(null);
   const numberInputRef = useRef(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(addContacts({ name, number }));
-    setName('');
-    setNumber('');
-    nameInputRef.current.focus();
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   dispatch(addContacts({ name, number }));
+  //   setName('');
+  //   setNumber('');
+  //   nameInputRef.current.focus();
+  // };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -24,6 +29,39 @@ function ContactForm() {
     } else if (name === 'number') {
       setNumber(value);
     }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    
+
+    const dublicateOfName = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    const dublicateOfNumber = contacts.some(
+      contact =>
+        contact.number.replace(/-/g, '').replace(/ /g, '') ===
+        number.replace(/ /g, '').replace(/-/g, '')
+    );
+
+    if (dublicateOfName) {
+      Swal.fire(`${name} is alredy in contacts`);
+      setName('');
+      setNumber('');
+      return false;
+    }
+
+    if (dublicateOfNumber) {
+      Swal.fire(`${number} is alredy in contacts`);
+      setName('');
+      setNumber('');
+      return false;
+    }
+    dispatch(addContacts({name, number}));
+    setName('');
+    setNumber('');
+    nameInputRef.current.focus();
   };
 
   return (
